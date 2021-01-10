9c300d039fa3d01f2238b306eb8181b6bb87f14d
Mitali Kaushal <mitali.kaushal9@gmail.com>
2021-01-10 13:35:21 -0800

    broken home page into smalkler components and styling the hover effect for homepage images

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import logo from './logo.svg';
+import {Route, Switch, BrowserRouter} from 'react-router-dom';
 import './App.css';
-import HomePage from './homepage-components';
+import HomePage from './pages/homepage/homepage-components';
+
+const HatsPage = () => (
+  <div>
+    <h2>HATS PAGE</h2>
+  </div>
+)
 
 function App() {
   return (
-   <HomePage/>
+    <div>
+      <BrowserRouter>
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route exact path='/hats' component={HatsPage} />
+      </Switch>
+      </BrowserRouter>
+    </div>
   );
 }
 
