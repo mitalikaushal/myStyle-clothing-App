2b2f37ac68ffa57a46c1de3793047fdc704fd3c5
Mitali Kaushal <mitali.kaushal9@gmail.com>
2021-01-12 09:28:32 -0800

    Header component functionality and styling

--- src/App.js
+++ src/App.js
@@ -3,13 +3,14 @@ import {Route, Switch, BrowserRouter} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage-components';
 import ShopPage from './pages/shop/shop.components';
-
+import Header from './components/header/header.component';
 
 
 function App() {
   return (
     <div>
       <BrowserRouter>
+      <Header/>
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route exact path='/shop' component={ShopPage} />
