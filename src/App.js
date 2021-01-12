e7a5acb7ea8e23cd1480db1c4d843d2269e99ca0
Mitali Kaushal <mitali.kaushal9@gmail.com>
2021-01-12 09:05:58 -0800

    added collection preview component and mapped the images

--- src/App.js
+++ src/App.js
@@ -2,12 +2,9 @@ import logo from './logo.svg';
 import {Route, Switch, BrowserRouter} from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/homepage/homepage-components';
+import ShopPage from './pages/shop/shop.components';
 
-const HatsPage = () => (
-  <div>
-    <h2>HATS PAGE</h2>
-  </div>
-)
+
 
 function App() {
   return (
@@ -15,7 +12,7 @@ function App() {
       <BrowserRouter>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route exact path='/hats' component={HatsPage} />
+        <Route exact path='/shop' component={ShopPage} />
       </Switch>
       </BrowserRouter>
     </div>
